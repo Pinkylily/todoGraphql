17c42dc78d412ddfe25c28b897f27f8e783dbb4e
Pinkylily <57408886+Pinkylily@users.noreply.github.com>
2024-02-13 16:39:10 +0100

    Delete src/Todo directory

--- src/Todo/data/TodoContext.ts
+++
@@ -1,8 +0,0 @@
-import { ITodoFilters } from "@todo/data/TodoTypes";
-import { createContext } from "react";
-
-const TodoContext = createContext<
-  [ITodoFilters, (todoFilters: ITodoFilters) => void]
->([{}, () => {}]);
-
-export default TodoContext;
--- src/Todo/pages/List/components/fallback/FallbackList.tsx
+++
@@ -1,5 +0,0 @@
-import ErrorFallback from "@/common/components/errorFallback/ErrorFallback";
-
-export const FallbackList: React.FC = () => (
-  <ErrorFallback message="La liste n'a pas pu être récupérée." />
-);
